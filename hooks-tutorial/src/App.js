8347474deb9b8634da70e82353f3857fa9889213
wlgud0402 <wlgudrlgus@naver.com>
2020-11-14 20:35:06 +0900

    useMemo

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -3,11 +3,13 @@ import Counter from "./components/Counter";
 import Info from "./components/Info";
 import Counter2 from "./components/Counter2";
 import Info2 from "./components/Info2";
+import Average from "./components/Average";
 
 const App = () => {
   const [visible, setVisible] = useState(false);
   return (
     <div>
+      <Average></Average>
       <Counter2></Counter2>
       <button
         onClick={() => {
