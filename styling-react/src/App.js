f42f4f16888a599cbd30409bae0310640e0b3950
wlgud0402 <wlgudrlgus@naver.com>
2020-11-17 17:11:38 +0900

    use module.css

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import SassComponent from "./SassComponent";
+import CSSModule from "./CSSModule";
 
 function App() {
   return (
     <div>
-      <SassComponent></SassComponent>
+      <CSSModule />
     </div>
   );
 }
