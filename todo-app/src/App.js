2d86d09f0823cb86f041e703e859c75f33498f6b
wlgud0402 <wlgudrlgus@naver.com>
2020-11-22 23:09:52 +0900

    finished todo app

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,8 +1,58 @@
 import './App.css';
 import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+import { useState, useRef, useCallback } from 'react';
 
 function App() {
-  return <TodoTemplate>ToDo App을 만들자!!</TodoTemplate>;
+  const [todos, setTodos] = useState([
+    { id: 1, text: '리액트의 기초 알아보기', checked: true },
+    { id: 2, text: '컴포넌트 스타일링 해보기', checked: false },
+    { id: 3, text: '일정관리 앱 만들어보기', checked: false },
+  ]);
+
+  //고유한 값으로 사용될 id => ref 를 사용하여 담는다
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1; //id값 1씩 증가
+    },
+    [todos],
+  );
+
+  //삭제
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos],
+  );
+
+  //수정
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
+
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+    </TodoTemplate>
+  );
 }
 
 export default App;
--- todo-app/src/components/TodoInsert.jsx
+++ todo-app/src/components/TodoInsert.jsx
@@ -1,1 +1,37 @@
-rsc;
+import React, { useState, useCallback } from 'react';
+import { MdAdd } from 'react-icons/md';
+import './TodoInsert.scss';
+
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue(''); //value값 초기화
+
+      //submit 이벤트는 브라우저의 새로고침을 발생시킨다. 방지하기위해 preventDefault실해
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
+  return (
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="할일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
+      <button type="submit">
+        <MdAdd />
+      </button>
+    </form>
+  );
+};
+
+export default TodoInsert;
