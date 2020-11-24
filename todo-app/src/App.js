302ccab6d7ef78f314d3666282570b62477bd705
wlgud0402 <wlgudrlgus@naver.com>
2020-11-24 23:39:12 +0900

    improve speed

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -4,48 +4,49 @@ import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import { useState, useRef, useCallback } from 'react';
 
-function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, text: '리액트의 기초 알아보기', checked: true },
-    { id: 2, text: '컴포넌트 스타일링 해보기', checked: false },
-    { id: 3, text: '일정관리 앱 만들어보기', checked: false },
-  ]);
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
 
+function App() {
+  const [todos, setTodos] = useState(createBulkTodos);
   //고유한 값으로 사용될 id => ref 를 사용하여 담는다
   const nextId = useRef(4);
+  // [{ id: 1, text: '리액트의 기초 알아보기', checked: true },
+  // { id: 2, text: '컴포넌트 스타일링 해보기', checked: false },
+  // { id: 3, text: '일정관리 앱 만들어보기', checked: false },]
 
-  const onInsert = useCallback(
-    (text) => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
-      setTodos(todos.concat(todo));
-      nextId.current += 1; //id값 1씩 증가
-    },
-    [todos],
-  );
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    setTodos((todos) => todos.concat(todo));
+    nextId.current += 1; //id값 1씩 증가
+  }, []);
 
   //삭제
-  const onRemove = useCallback(
-    (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
-    },
-    [todos],
-  );
+  const onRemove = useCallback((id) => {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }, []);
 
   //수정
-  const onToggle = useCallback(
-    (id) => {
-      setTodos(
-        todos.map((todo) =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
-    },
-    [todos],
-  );
+  const onToggle = useCallback((id) => {
+    setTodos((todos) =>
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+      ),
+    );
+  }, []);
 
   return (
     <TodoTemplate>
